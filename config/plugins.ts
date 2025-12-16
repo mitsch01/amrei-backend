a7910eed0fbcbe94aba205b0aba0c78e9c6bf2f2
mitsch01 <schwartz.miriam@gmail.com>
2025-12-16 13:55:18 +0100

    updated plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,21 +3,23 @@ export default ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("R2_ACCESS_KEY_ID"),
-        secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
-        region: env("R2_REGION", "auto"), // R2 uses "auto"
-        endpoint: env("R2_ENDPOINT"), // e.g. https://<accountid>.r2.cloudflarestorage.com
-        s3ForcePathStyle: true, // required for R2
-        signatureVersion: "v4",
+        s3Options: {
+          accessKeyId: env("R2_ACCESS_KEY_ID"),
+          secretAccessKey: env("R2_SECRET_ACCESS_KEY"),
+          region: env("R2_REGION", "auto"), // R2 uses "auto"
+          endpoint: env("R2_ENDPOINT"), // e.g. https://<accountid>.r2.cloudflarestorage.com
+          s3ForcePathStyle: true, // required for R2
+          signatureVersion: "v4",
+        },
         params: {
-          Bucket: env("R2_BUCKET")
-        }
+          Bucket: env("R2_BUCKET"),
+        },
       },
       actionOptions: {
         upload: {},
         uploadStream: {},
-        delete: {}
-      }
-    }
-  }
+        delete: {},
+      },
+    },
+  },
 });
