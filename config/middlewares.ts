e8867f2343146231307657cbb3c4bbba2eeeb88c
mitsch01 <schwartz.miriam@gmail.com>
2026-01-05 14:56:49 +0100

    updated middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,23 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": ["'self'", "data:", "blob:", "https://market-assets.strapi.io", "https://pub-b3ccf8d734b54cc1b3cfecaa98da0e26.r2.dev", "https://*.r2.dev", "https://*.r2.cloudflarestorage.com"],
+          "connect-src": ["'self'", "https://pub-b3ccf8d734b54cc1b3cfecaa98da0e26.r2.dev", "https://*.r2.dev", "https://*.r2.cloudflarestorage.com"]
+        }
+      }
+    }
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public"
 ];
